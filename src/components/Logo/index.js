39b364bcf34c39924c1416308351cd6ef199a5b1
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-11 21:14:01 -0300

    criado componente logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+import { LogoWrapper } from "./styles";
+
+export default class Logo extends Component {
+  render() {
+    return <LogoWrapper>JeniCarvalho {"</>"}</LogoWrapper>;
+  }
+}
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LogoWrapper = styled.h1`
+  display: flex;
+  justify-content: center;
+  padding: 15px 0;
+  font-size: 40px;
+  color: #000;
+`;
