82c96189a419d08d96887c5048bccf917171af65
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-11 21:13:38 -0300

    criado componente copyright

---
+++ src/components/Layout/Footer/Copyright/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+export default function Copyright() {
+  return (
+    <Container>
+      <span>JeniCarvalho {"</>"} Conteúdo Para Devs</span> 👩‍💻
+    </Container>
+  );
+}
---
+++ src/components/Layout/Footer/Copyright/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f6f6f6;
+  padding: 10px 0;
+
+  span {
+    font-size: 12px;
+  }
+`;
