01e698cac10781fa141b5641ad68dcfe99af3d8e
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-11 21:13:53 -0300

    criado componente  header

---
+++ src/components/Layout/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Logo from "../../Logo";
+import Menu from "../../Menu";
+
+export default function Header() {
+  return (
+    <>
+      <Logo />
+      <Menu />
+    </>
+  );
+}
