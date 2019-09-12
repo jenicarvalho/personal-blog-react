089d15391867d22c8a4ff6e3b34c3bce14c5ae11
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-11 21:12:57 -0300

    pagina home

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import Category from "../../components/Category";
+// import { Container } from './styles';
+
+export default class Home extends Component {
+  render() {
+    return <Category name="React" />;
+  }
+}
