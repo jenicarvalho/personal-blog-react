a8b4ac7b1b55969d8eb26095af9265ba16c76ca7
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-17 20:38:49 -0300

    alterado cor do background

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 export const NavBar = styled.nav`
-  border-top: 1px solid #e8e8e8;
+  border-top: 1px solid #ebebeb6e;
   padding: 15px 0;
   height: 58px;
-
+  background: #fff;
+  box-shadow: 0px 5px 15px #54545414;
+  margin-bottom: 30px;
   div {
     display: flex;
     justify-content: space-between;
