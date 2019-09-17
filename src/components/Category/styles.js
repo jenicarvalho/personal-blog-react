67af7353701360f93121f8a8a9777700d3178b10
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-17 20:34:20 -0300

    criado componente category

--- src/components/Category/styles.js
+++ src/components/Category/styles.js
@@ -2,17 +2,19 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   h2 {
+    color: #393e46;
     border-bottom: 2px solid #eee;
     position: relative;
-    padding-bottom: 5px;
+    letter-spacing: 1px;
     text-transform: uppercase;
-    font-size: 16px;
+    font-size: 13px;
+    padding: 14px 0;
 
     &:after {
       content: "";
       position: absolute;
-      border-bottom: 3px solid #e91e63;
-      width: 20%;
+      border-bottom: 3px solid var(--primary);
+      width: 15%;
       left: 0;
       bottom: -3px;
     }
