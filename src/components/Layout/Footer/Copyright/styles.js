fe56367a0292934c4844b63bdc03c4b3d5f43df9
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-17 20:35:44 -0300

    adicionado background color

--- src/components/Layout/Footer/Copyright/styles.js
+++ src/components/Layout/Footer/Copyright/styles.js
@@ -4,10 +4,10 @@ export const Container = styled.footer`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #f6f6f6;
   padding: 10px 0;
 
   span {
     font-size: 12px;
+    color: #fff;
   }
 `;
