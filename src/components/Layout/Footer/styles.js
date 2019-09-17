34303ae6e60ef4daab4bbcc12bac2bd5d2e285e7
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-17 20:34:52 -0300

    alterada cor do background do rodape

--- src/components/Layout/Footer/styles.js
+++ src/components/Layout/Footer/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Base } from "../Base";
 
 export const Container = styled.div`
-  background: #262626;
+  background: #191919;
   padding: 40px;
   color: #fff;
   font-size: 12px;
@@ -35,7 +35,7 @@ export const LastNews = styled.div`
     display: block;
 
     &:hover {
-      color: #e91e63;
+      color: var(--primary);
     }
   }
 
