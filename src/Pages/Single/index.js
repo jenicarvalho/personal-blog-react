90b88c15cc379b0945d039115ee6e4bac362ace0
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-27 21:46:01 -0300

    adicionada pagina single

---
+++ src/Pages/Single/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+// import { Container } from './styles';
+
+export default class Single extends Component {
+  render() {
+    return <h1>single</h1>;
+  }
+}
