8f039b4f2028c82081051cd41cb23c8b456923c3
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-27 21:43:21 -0300

    removido components nao utilizados

--- src/components/ArticleList/index.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const ArticleList = styled.div`
-  display: grid;
-  grid-template-columns: repeat(${props => (props.grid ? props.grid : 3)}, 1fr);
-  grid-gap: 30px;
-  margin: 30px 0;
-`;
