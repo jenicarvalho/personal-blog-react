04244484e9957717de848ae74eede02093c9b941
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-29 17:25:14 -0300

    adicionado conteudo da single e estilizado

--- src/Pages/Single/index.js
+++ src/Pages/Single/index.js
@@ -1,9 +1,61 @@
 import React, { Component } from "react";
-
-// import { Container } from './styles';
+import { Link } from "react-router-dom";
+import { FiTag } from "react-icons/fi";
+import Article from "../../components/Article";
+import { Container, Tags, Image, Content, Meta, MorePosts } from "./styles";
 
 export default class Single extends Component {
   render() {
-    return <h1>single</h1>;
+    return (
+      <Container>
+        <header>
+          <Tags>
+            <FiTag />
+            <Link to="/">React</Link>
+            <Link to="/">JavaScript</Link>
+          </Tags>
+          <h1>
+            Tipos de navegação no React Native Lorem ipsum dolor sit ammet
+          </h1>
+          <p>
+            The right way to create your build pipeline in Docker and reduce the
+            size of your images
+          </p>
+          <Meta>20 de Agosto de 2019</Meta>
+        </header>
+        <Image>
+          <img
+            src="https://blog.rocketseat.com.br/content/images/2019/07/Tipos-de-navegac-a-o-no-React-Native.png"
+            alt="hero"
+          />
+        </Image>
+
+        <Content>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam nam
+          enim itaque rem nulla delectus, quae fugiat iusto, unde earum a
+          consequatur architecto at natus, non distinctio tenetur praesentium
+          accusamus. A id quisquam unde recusandae, molestias, ullam, nobis
+          dolores distinctio facilis quia quis dignissimos! Explicabo quisquam
+          iure, amet atque illo ipsam itaque in unde vel asperiores eaque aut,
+          eos commodi. Labore porro ab saepe eligendi, <Link>consectetur</Link>{" "}
+          praesentium! Ut eos placeat amet veritatis tempore earum reiciendis,
+          beatae est excepturi? Modi, distinctio eos a aut qui sapiente repellat
+          fugiat animi corporis corrupti! Nulla delectus porro mollitia
+          asperiores
+          <h2>Workflow Context API</h2>
+          deserunt vitae doloribus, dicta facere magnam, velit corporis quae
+          molestias iste dolores <Link>cumque minima quos</Link> animi. Veniam,
+          perferendis! Hic ut corporis eveniet placeat maxime temporibus!
+        </Content>
+
+        <MorePosts>
+          <h2>Veja também</h2>
+          <section>
+            <Article />
+            <Article />
+          </section>
+        </MorePosts>
+      </Container>
+    );
   }
 }
--- src/Pages/Single/styles.js
+++ src/Pages/Single/styles.js
@@ -1,3 +1,90 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  header {
+    max-width: 760px;
+    margin: 0 auto;
+
+    h1 {
+      font-size: 40px;
+      line-height: 45px;
+      color: #333;
+      margin-bottom: 0;
+    }
+
+    p {
+      font-size: 22px;
+      line-height: 30px;
+    }
+  }
+`;
+export const Tags = styled.div`
+  display: flex;
+  align-items: center;
+
+  svg {
+    margin-right: 10px;
+    color: var(--secondary);
+    font-size: 18px;
+  }
+
+  a {
+    background-color: #f4f4f4;
+    font-size: 10px;
+    font-weight: 700;
+    display: inline-block;
+    margin: 0 2px;
+    padding: 4px 10px;
+    -webkit-transition: all 0.15s ease;
+    transition: all 0.15s ease;
+    letter-spacing: 1.5px;
+    text-transform: uppercase;
+    border-radius: 5px;
+
+    &:hover {
+      background-color: var(--primary);
+      color: #fff;
+    }
+  }
+`;
+
+export const Image = styled.div`
+  width: 100%;
+
+  img {
+    width: 100%;
+  }
+`;
+
+export const Content = styled.div`
+  max-width: 760px;
+  margin: 50px auto;
+  font-size: 18px;
+  line-height: 32px;
+
+  a {
+    padding: 2px;
+    border-bottom: 1px solid var(--secondary);
+
+    &:hover {
+      background-color: var(--secondary);
+      color: #fff;
+      text-decoration: none;
+    }
+  }
+`;
+export const Meta = styled.div`
+  color: rgba(0, 0, 0, 0.54);
+  margin-bottom: 20px;
+`;
+
+export const MorePosts = styled.div`
+  max-width: 760px;
+  margin: 30px auto 50px;
+
+  section {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 30px;
+  }
+`;
