e53d86ac5f5883ba6ed61a9176d6ca68c41b1e2a
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-29 15:33:52 -0300

    adicionado border bottom no menu

--- src/components/Layout/Header/Menu/styles.js
+++ src/components/Layout/Header/Menu/styles.js
@@ -1,25 +1,24 @@
 import styled from "styled-components";
 
 export const NavBar = styled.nav`
-  border-top: 1px solid #ebebeb6e;
+  border-bottom: 1px solid #eee;
   padding: 15px 0;
   height: 58px;
-  background: #fff;
-  box-shadow: 0px 5px 15px #54545414;
   margin-bottom: 30px;
-  
+
   div {
     display: flex;
     justify-content: space-between;
   }
-  
+
   a {
-  color: #201f22;
-  text-transform: uppercase;
-  font-size: 12px;
-  letter-spacing: 1px;
-  padding: 5px 10px;
-  transition: all 0.2s ease-in-out;
+    color: #201f22;
+    text-transform: uppercase;
+    font-size: 12px;
+    letter-spacing: 1px;
+    padding: 5px 10px;
+    transition: all 0.2s ease-in-out;
+    font-weight: 700;
 
     &:hover {
       background: var(--primary);
