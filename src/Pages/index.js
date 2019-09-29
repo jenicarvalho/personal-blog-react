bc05e1938976b26600107d946d69b07f3e29b139
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-29 17:24:31 -0300

    removido componente base

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,14 +2,13 @@ import React, { Component } from "react";
 
 import Header from "../components/Layout/Header";
 import Footer from "../components/Layout/Footer";
-import { Base } from "../components/Layout/Base";
 
 export default class Default extends Component {
   render() {
     return (
       <>
         <Header />
-        <Base>{this.props.children}</Base>
+        {this.props.children}
         <Footer />
       </>
     );
