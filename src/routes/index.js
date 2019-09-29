dec013b5b6ae4c4a4115d491ace4fc2b73c91efb
Jeniffer Carvalho <jenicarvalho@live.com>
2019-09-29 18:10:43 -0300

    adicionada rota about

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Default from "../Pages";
 import Home from "../Pages/Home";
 import Single from "../Pages/Single";
+import About from "../Pages/About";
 
 const Routes = () => {
   return (
@@ -12,7 +13,8 @@ const Routes = () => {
       <Switch>
         <Default>
           <Route exact path="/" component={Home} />
-          <Route path="/:post" component={Single} />
+          <Route path="/post/:post" component={Single} />
+          <Route path="/about" component={About} />
         </Default>
       </Switch>
     </BrowserRouter>
