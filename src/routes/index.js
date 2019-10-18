45b282db9eb53cf33c7c48ecad196a1307c603a4
Jeniffer Carvalho <jenicarvalho@live.com>
2019-10-17 21:02:33 -0300

    adicionada rota para categoria

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "../Pages/Home";
 import Single from "../Pages/Single";
 import About from "../Pages/About";
+import CategoryList from "../Pages/CategoryList";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/about" component={About} />
+        <Route path="/categoria" component={CategoryList} />
         <Route exact path="/" component={Home} />
         <Route path="/:postslug" component={Single} />
       </Switch>
