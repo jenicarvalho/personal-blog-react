78aa79ca7c36cb0fff6f7ed19a54d5c0d5f5b583
Jeniffer Carvalho <jenicarvalho@live.com>
2019-10-19 14:53:43 -0300

    criado componente pagination

--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -6,3 +6,24 @@ export const ArticleContainer = styled.div`
   grid-gap: 20px;
   margin: 20px 0;
 `;
+
+export const Pagination = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+
+  span {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid;
+    margin-right: 10px;
+    width: 25px;
+    height: 25px;
+
+    &:hover {
+      background: #000;
+      color: #fff;
+    }
+  }
+`
