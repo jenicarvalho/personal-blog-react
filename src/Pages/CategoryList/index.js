26b34aa05246f82f1c59c86052d266e6330b445d
Jeniffer Carvalho <jenicarvalho@live.com>
2019-10-19 19:01:14 -0300

    adicionado integracao com a categoria

--- src/Pages/CategoryList/index.js
+++ src/Pages/CategoryList/index.js
@@ -10,33 +10,60 @@ import Footer from "../../components/Layout/Footer";
 
 export default class CategoryList extends Component {
   state = {
-    categoryPosts: []
+    categoryPosts: [],
+    categoryId: null,
+    categoryName: ''
   };
 
   componentDidMount = async () => {
+    const { category, name } = this.props.match.params;
+    this.setState({ categoryId: category, categoryName: name})
+    this.requestCategory(category)
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { category, name } = this.props.match.params;  
+    
+    if(prevProps === undefined) {
+      return false
+    }
+    
+    if (this.state.categoryId !== category) {
+      this.setState({ categoryId: category, categoryName: name})
+      this.requestCategory(category)
+    }
+  }
+
+  requestCategory = async category => {
     await axios
-      .get("http://jenicarvalho.com.br/?rest_route=/wp/v2/posts/&categories=4")
+      .get(`http://jenicarvalho.com.br/wp-json/wp/v2/posts?categories=${category}`)
       .then(posts => {
         this.setState({
           categoryPosts: posts.data
         });
       });
-  };
+  }
 
   render() {
+    const {  categoryPosts, categoryName } = this.state;
+
     return (
       <>
         <Header />
         <Base>
-          <Category name="Categoria React" />
+          <Category name={`Categoria ${categoryName}`} />
           <ArticleContainer>
-            {this.state.categoryPosts.map(post => (
-              <Article
-                content={post}
-                categoryId={post.categories}
-                key={post.id}
-              />
-            ))}
+            { Array.isArray(categoryPosts) && categoryPosts.length ? 
+              categoryPosts.map(post => (
+                <Article
+                  content={post}
+                  categoryId={post.categories}
+                  key={post.id}
+                />
+              ))
+              : 
+              ( <p>Sem artigos nessa categoria ainda! :( </p> )
+            }
           </ArticleContainer>
         </Base>
         <Footer />
