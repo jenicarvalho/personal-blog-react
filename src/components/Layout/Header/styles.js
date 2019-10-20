d517f3d97ede483c3e1a7d0d7d6a4bbdb2353cee
Jeniffer Carvalho <jenicarvalho@live.com>
2019-10-20 17:27:08 -0300

    estilo do menu sandwich

---
+++ src/components/Layout/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  header {
+    margin: 0 30px;
+    svg {
+      font-size: 30px;
+      cursor: pointer
+    }
+  }
+`;
