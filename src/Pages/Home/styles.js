43522af3c55d92fb917116d0a6af62daeb40e5f5
Jeniffer Carvalho <jenicarvalho@live.com>
2019-10-20 17:25:24 -0300

    responsividade na home

--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -1,15 +1,29 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const ArticleContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-gap: 20px;
-  margin: 20px 0;
+  grid-gap: 30px;
+  margin: 30px 0;
+
+  ${media.lessThan("medium")`
+    grid-template-columns: repeat(2, 1fr);
+    margin: 30px;
+  `}
+  
+  ${media.lessThan("600px")`
+    grid-template-columns: repeat(1, 1fr);
+  `}
 `;
 
 export const Pagination = styled.div`
   display: flex;
   margin-bottom: 20px;
+
+  ${media.lessThan("medium")`
+    margin: 30px;
+  `}
 
   span {
     cursor: pointer;
