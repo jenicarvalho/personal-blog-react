7a47fe03fa749e1d657e24381aefed232f0337b7
Jeniffer Carvalho <jenicarvalho@live.com>
2019-10-20 17:26:39 -0300

    adicionado menu sandwich

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
+import {FiMenu} from 'react-icons/fi'
 import Logo from "./Logo";
 import Menu from "./Menu";
+import { Container } from './styles'
 
 export default class Header extends Component {
+  state = {
+    showMobileMenu: false,
+    showSandwich: false,
+  }
+  
+  componentDidMount = () => {
+    if (window.innerWidth <= 768) {
+      this.setState({ showSandwich: true });
+    }
+  };
+
+  handleMenuMobile = () => {
+    this.setState({showMobileMenu: !this.state.showMobileMenu})
+  }
+  
   render() {
+    const {showMobileMenu, showSandwich} = this.state;
     return (
-      <header>
-        <Logo />
-        <Menu />
-      </header>
+      <Container>
+        <header>
+          <Logo />
+          {showSandwich && <FiMenu onClick={() => this.handleMenuMobile()} />}
+          <Menu responsive={showMobileMenu}/>
+        </header>
+      </Container>
     );
   }
 }
