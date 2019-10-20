00af2c1fe71014010166808f14f2235a13e67784
Jeniffer Carvalho <jenicarvalho@live.com>
2019-10-20 15:26:19 -0300

    criada funcao para remover tags html

--- src/Helpers/functions.js
+++ src/Helpers/functions.js
@@ -1,3 +1,7 @@
 export function sliceString(text, count) {
   return text.slice(0, count) + (text.length > count ? "..." : "");
 }
+
+export function stripedHtml(text) {
+  return text.replace(/<[^>]+>/g, '');
+}
