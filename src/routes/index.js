4c3a3dbe87c2d5c96385d06c088bab4e0aa72c44
Jeniffer Carvalho <jenicarvalho@live.com>
2019-10-20 15:27:23 -0300

    removido rota para pagina about

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 /** Paginas */
 import Home from "../Pages/Home";
 import Single from "../Pages/Single";
-import About from "../Pages/About";
 import CategoryList from "../Pages/CategoryList";
 
 const Routes = () => {
